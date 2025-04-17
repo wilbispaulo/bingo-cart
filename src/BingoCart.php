dc7ae08baa7198f2312e36809a0b53b2b039cb0d
wilbispaulo <wilbispaulo@hotmail.com>
2025-04-17 13:13:11 -0300

    Alterações no hash com public cert

--- src/BingoCart.php
+++ src/BingoCart.php
@@ -2,14 +2,15 @@
 
 namespace BingoCart;
 
-use BingoCart\library\HashSSl;
-
 class BingoCart
 {
     private $wordSeries = [];
+    private string $privatePEM;
+    private string $publicPEM;
 
     public function __construct(
-        private HashSSl $hashSSL,
+        private $pathToP12,
+        private $secretCert,
         private $date,
         private $serie,
         private $owner,
@@ -17,6 +18,17 @@ class BingoCart
         private $titleEvent,
         private $txtJackpot
     ) {
+        $certPEM = [];
+        $this->privatePEM = '';
+        if (count($p12 = glob($this->pathToP12)) > 0) {
+            $certP12 = file_get_contents($p12[0]);
+            openssl_pkcs12_read($certP12, $certPEM, $this->secretCert);
+            $this->privatePEM = $certPEM['pkey'];
+            $this->publicPEM = openssl_pkey_get_details(openssl_pkey_get_public($certPEM['cert']))['key'];
+        } else {
+            throw new \Exception('Private key not found');
+        }
+        // $this->hashKey = base64_encode(hash('SHA512', $this->privatePEM, true));
         $this->makeHead();
         $this->wordSeries['carts'] = [];
     }
@@ -44,133 +56,19 @@ class BingoCart
         return $this->wordSeries;
     }
 
-
-    // Test a 25 element array if contents is numeric
-    public static function isNumCart(array $cart): bool
-    {
-        foreach ($cart as $ball) {
-            $isNum = is_int($ball);
-        }
-        return $isNum;
-    }
-
-    // Get a hmac SHA1 hash string from a lenght 81 string cart
-    public static function getHashCart(string $hashNumWordCard): string
-    {
-        return substr($hashNumWordCard, -28);
-    }
-
-    // Generate a sorted string from 25 numeric | string (lenght 2) elements array ($cart)
-    public static function wordSortCart(array $cart): string
-    {
-        if (self::isNumCart($cart) === false) {
-            $cart = self::strToIntCart($cart);
-        }
-        sort($cart, SORT_NUMERIC);
-        return self::toWordCart($cart);
-    }
-
-    // Split a lenght 50 string, extracted from lenght 97 string ($hashNumWord), in to a 25 numeric elements array
-    public static function splitWord(string $hashNumWord): array
-    {
-        $wordCart = substr($hashNumWord, 3, 50);
-        $cart = str_split($wordCart, 2);
-        $cart = self::strToIntCart($cart);
-        return $cart;
-    }
-
-    // Convert a 25 string elements array in to a int elements array
-    public static function strToIntCart(array $cart): array
-    {
-        $cart = array_map(function ($conv) {
-            return intval($conv);
-        }, $cart);
-        return $cart;
-    }
-
-    // Convert 25 numeric elements array in a string (lenght 50)
-    public static function toWordCart(array $cart): string
-    {
-        $wordCart = "";
-        foreach ($cart as $ball) {
-            $wordCart .= sprintf("%02d", $ball);
-        }
-        return $wordCart;
-    }
-
-    public function signJson(string $json, string $privateKey = $this->hashSSL->getPrivatePEM())
+    public function signJson(string $json)
     {
         $sign = '';
-        openssl_sign($json, $sign, $privateKey, OPENSSL_ALGO_SHA512);
+        openssl_sign($json, $sign, $this->privatePEM, OPENSSL_ALGO_SHA512);
         $array = json_decode($json, true);
         $array['sign_sha512'] = base64_encode($sign);
         return json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     }
 
-    public static function validateSignJson(string $json, string $pathToCert): bool
-    {
-        $publicKey = openssl_pkey_get_details(openssl_pkey_get_public(file_get_contents($pathToCert)))['key'];
-        $serie = json_decode($json, true);
-
-        // VALIDATE KEY
-        if (!array_key_exists('sign_sha512', $serie)) {
-            return 0;
-        }
-
-        // VALIDATE SIGN
-        $sign['sign_sha512'] = $serie['sign_sha512'];
-
-        $data = json_encode(array_diff_assoc($serie, $sign), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-
-        $signOk = openssl_verify($data, base64_decode($sign['sign_sha512']), $publicKey, OPENSSL_ALGO_SHA512);
-
-        return ($signOk === 1);
-    }
-
-    // Update hmac hash with SHA512 algo using KEY_HMAC key
-    private function updateHashSeries()
-    {
-        $this->wordSeries['head']['hash'] = $this->generateHashSeries($this->wordSeries);
-    }
-
-
-    // Check a cart series if is valid using hmac hash sha512 algo
-    public function checkSeries(array $series): bool
-    {
-        $hash = $this->generateHashSeries($series);
-        if ($hash === $series['head']['hash']) return true;
-        return false;
-    }
-
-    // Check a lenght 81 string is valid cart using hmac hash SHA1 algo
-    public function checkCart(string $hashNumWordCart): bool
-    {
-        $wordCart = substr($hashNumWordCart, 0, 53);
-        $hashCart = self::getHashCart($hashNumWordCart);
-        if (false === ($this->hashCart($wordCart) === $hashCart)) return false;
-        return true;
-    }
-
     // Generate hmac hash with SHA1 algo using KEY_HMAC key
     public function hashCart(string $word): string
     {
-        return base64_encode($this->hashSSL->Hash('sha1', $word, true));
-    }
-
-    // Generate hmac hash with SHA512 algo using KEY_HMAC key
-    public function generateHashSeries(array $series): string
-    {
-        $hash = $this->hashSSL->HashInit('sha512', $this->hashSSL->getSecret());
-        foreach ($series['head'] as $index => $line) {
-            if ($index !== 'hash') hash_update($hash, $line);
-        }
-        if (isset($series['carts'])) {
-            foreach ($series['carts'] as $line) {
-                hash_update($hash, $line);
-            }
-        }
-
-        return base64_encode(hex2bin(hash_final($hash)));
+        return base64_encode(hash_hmac('sha1', $word, $this->publicPEM));
     }
 
     public function getPrefixSerial(): string
@@ -207,8 +105,6 @@ class BingoCart
     {
         return $this->wordSeries;
     }
-
-    // Set a private prefix serial ($prefixSerial) with date and serie
 
     public function setDate(string $date)
     {
@@ -303,5 +199,94 @@ class BingoCart
         $prefixSerial .= substr($this->serie, 0, 2);
 
         $this->wordSeries['head']['prefix_serial'] = $prefixSerial;
+    }
+
+    // Test a 25 element array if contents is numeric
+    public static function isNumCart(array $cart): bool
+    {
+        foreach ($cart as $ball) {
+            $isNum = is_int($ball);
+        }
+        return $isNum;
+    }
+
+    // Get a hmac SHA1 hash string from a lenght 81 string cart
+    public static function getHashCart(string $hashNumWordCard): string
+    {
+        return substr($hashNumWordCard, -28);
+    }
+
+    // Generate a sorted string from 25 numeric | string (lenght 2) elements array ($cart)
+    public static function wordSortCart(array $cart): string
+    {
+        if (self::isNumCart($cart) === false) {
+            $cart = self::strToIntCart($cart);
+        }
+        sort($cart, SORT_NUMERIC);
+        return self::toWordCart($cart);
+    }
+
+    // Split a lenght 50 string, extracted from lenght 97 string ($hashNumWord), in to a 25 numeric elements array
+    public static function splitWord(string $hashNumWord): array
+    {
+        $wordCart = substr($hashNumWord, 3, 50);
+        $cart = str_split($wordCart, 2);
+        $cart = self::strToIntCart($cart);
+        return $cart;
+    }
+
+    // Convert a 25 string elements array in to a int elements array
+    public static function strToIntCart(array $cart): array
+    {
+        $cart = array_map(function ($conv) {
+            return intval($conv);
+        }, $cart);
+        return $cart;
+    }
+
+    // Convert 25 numeric elements array in a string (lenght 50)
+    public static function toWordCart(array $cart): string
+    {
+        $wordCart = "";
+        foreach ($cart as $ball) {
+            $wordCart .= sprintf("%02d", $ball);
+        }
+        return $wordCart;
+    }
+
+    // Check a lenght 81 string is valid cart using hmac hash SHA1 algo
+    public static function checkCart(string $hashNumWordCart, string $pathToCert): bool
+    {
+        if (count($p12 = glob($pathToCert)) > 0) {
+            $publicKey = openssl_pkey_get_public(file_get_contents($p12[0]));
+            $publicPEM = openssl_pkey_get_details($publicKey)['key'];
+        } else {
+            throw new \Exception('Public certificate not found');
+        }
+        $wordCart = substr($hashNumWordCart, 0, 53);
+        $hashCart = self::getHashCart($hashNumWordCart);
+        $hash = base64_encode(hash_hmac('sha1', $wordCart, $publicPEM));
+        if (false === ($hash === $hashCart)) return false;
+        return true;
+    }
+
+    public static function validateSignJson(string $json, string $pathToCert): bool
+    {
+        $publicKey = openssl_pkey_get_details(openssl_pkey_get_public(file_get_contents($pathToCert)))['key'];
+        $serie = json_decode($json, true);
+
+        // VALIDATE KEY
+        if (!array_key_exists('sign_sha512', $serie)) {
+            return 0;
+        }
+
+        // VALIDATE SIGN
+        $sign['sign_sha512'] = $serie['sign_sha512'];
+
+        $data = json_encode(array_diff_assoc($serie, $sign), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        $signOk = openssl_verify($data, base64_decode($sign['sign_sha512']), $publicKey, OPENSSL_ALGO_SHA512);
+
+        return ($signOk === 1);
     }
 }
